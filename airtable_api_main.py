95bb01db3081a33f51f8f2481f36c50ef9f81ed1
Tim Karthaus <tim.karthaus@telekom.de>
2023-11-20 14:18:02 +0100

    First upload of real files

--- airtable_api_main.py
+++ airtable_api_main.py
@@ -1,1 +1,130 @@
-#test
+from pyairtable import Api
+from pyairtable import utils
+import requests
+import json
+import os
+
+def request_new(furl):
+    requestBody = {
+        "filter":{
+            "domain":[
+                furl
+                ]
+        }
+    }
+
+    res = requests.post(requestUrl, headers={'accessToken': accessToken}, json=requestBody)
+    result = res.json()
+    # Ist die Anfrage leer? Dann nur url drucken
+    if result["result"]=="":
+        table.create({
+            "URL": furl,
+        })
+    else:
+        for company in result.get("result", []):
+            name = company.get('name', '')
+            url = company.get('domain', '')
+    
+            try:
+                desc_l = company['description']['long']
+            except (KeyError, TypeError):
+                desc_l = ''
+
+            try:
+                desc_s = company['description']['short']
+            except (KeyError, TypeError):
+                desc_s = ''
+
+            try:
+                moneyraised = company['totalMoneyRaised']['totalAmount']['amount']
+            except (KeyError, TypeError):
+                moneyraised = 0
+            try:
+                foundedYear = company['foundedYear']
+            except(KeyError, TypeError):
+                foundedYear = '1000'
+
+            companyStage = company.get('stage', '')
+
+            try:
+                img = company['logos']['imageUrl']
+            except (KeyError, TypeError):
+                img = ''
+
+            try:
+                country = company['location']['country']
+            except (KeyError, TypeError):
+                country = ''
+            
+            try:
+                tracxn_score = company['tracxnScore']
+            except (KeyError, TypeError):
+                tracxn_score = 0    
+
+            categories = []
+            for businessModels in company.get("businessModelList", []):
+                try:
+                    categories.append(businessModels['name'])
+                except(KeyError, TypeError):
+                    test = ''
+        
+            table.create({
+            
+            "Name": name,
+            "URL": url,
+            "Description (long)": desc_l,
+            "Description (short)": desc_s,
+            "Money raised": moneyraised,
+            "Founded year": str(foundedYear) + "-01-01",
+            "Company Stage": companyStage,
+            "Logo": [utils.attachment(img)] if img else [],
+            "Country": country,
+            "Tracxn Score": tracxn_score
+            })
+
+def update():
+    result = table.all(fields=['URL'])
+    
+
+    for url in result:
+        requestBody = {
+        "filter":{
+            "domain":[
+                url["fields"]["URL"]
+                ]
+            }
+        } 
+        tracxn_res = requests.post(requestUrl, headers={'accessToken': accessToken}, json=requestBody)
+        tracxn_result = tracxn_res.json()
+        for company in tracxn_result.get("result", []):
+            try:
+                country = company['location']['country']
+            except (KeyError, TypeError):
+                country = ''
+            
+            try:
+                tracxn_score = company['tracxnScore']
+            except (KeyError, TypeError):
+                tracxn_score = 0
+            
+        table.update(url["id"], {
+            "Country": country,
+            "Tracxn Score": tracxn_score
+        })
+            
+
+
+
+
+api = Api(os.environ.get('API_KEY_AIRTABLE'))
+table = api.table("appIQwr4DelJi0HRI", "tblxZB7j9cgrLFk7q")
+requestUrl = "https://tracxn.com/api/2.2/companies"
+accessToken = os.environ.get('API_KEY_TRACXN')
+
+
+
+array = ['hiya.com']
+
+
+for i in array:
+    request_new(i)
