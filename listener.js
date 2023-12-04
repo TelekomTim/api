4e6c936249716cc3d62269480c02effd40d2ecbf
Tim Karthaus <tim.karthaus@telekom.de>
2023-12-04 11:23:56 +0100

    First try

---
+++ listener.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/endpoint', (req, res) => {
+  const receivedString = req.body.data;
+  console.log('Received String:', receivedString);
+
+  // Process the received string as needed
+
+  res.json({ message: 'String received successfully' });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
